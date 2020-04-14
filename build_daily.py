58afb6529aaeb9c744939aa857133ef4e01c107d
Lubos Dolezel <lubos@dolezel.info>
2020-04-14 15:00:11 +0200

    Add a Python script to download the latest DEB and build & push a Docker image based on it

---
+++ build_daily.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import requests
+import re
+import subprocess
+from xml.etree import ElementTree
+
+lastBuildID = requests.get('https://jenkins.darlinghq.org/job/darling-ubuntu-latest/lastSuccessfulBuild/buildNumber').text
+
+areq = requests.get('http://darling-daily.s3.eu-central-1.amazonaws.com/?delimiter=/&prefix=jobs/darling-ubuntu-latest/' + lastBuildID + '/')
+artifactsListing = ElementTree.fromstring(areq.content)
+
+namespaces = {'s3': 'http://s3.amazonaws.com/doc/2006-03-01/'}
+keys = artifactsListing.findall('./s3:Contents/s3:Key', namespaces)
+
+for key in keys:
+    text = key.text
+    if "/darling_" in text:
+        artifactUrl = 'http://darling-daily.s3.eu-central-1.amazonaws.com/' + text
+        break
+
+if not artifactUrl:
+    print("Artifact not found")
+    sys.exit(1)
+
+print("Artifact URL: " + artifactUrl)
+reVersion = re.compile('/darling_([^_]+)')
+match = reVersion.search(artifactUrl)
+
+if not match:
+    print("Artifact version cannot be parsed")
+    sys.exit(1)
+
+version = match.group(1)
+
+tag = "darlinghq/darling:v" + version
+
+subprocess.run(["docker", "build", "--build-arg", "DARLING_DEB=" + artifactUrl, "-t", tag, "."], check=True)
+subprocess.run(["docker", "tag", tag, "darlinghq/darling:latest"], check=True)
+subprocess.run(["docker", "push", tag])
+subprocess.run(["docker", "push", "darlinghq/darling:latest"])
+
